f990d48d5c0026e25fb8ca2adfce560f05b9aad4
frabrenn <frabrenn@amazon.com>
2023-05-29 18:15:28 -0600

    hello friend

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
